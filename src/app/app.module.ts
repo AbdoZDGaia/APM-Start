be2c0fd69c59d55260f7f1a8a93592ccc265a9c6
abdozdgaia <oldtimeprogrammer@yahoo.com>
2020-11-28 14:56:37 +0200

    http and observable object added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,12 @@ import { ProductListComponent } from './products/product-list.component'
 import { StarComponent } from './shared/star.component'
 import { FormsModule } from '@angular/forms';
 import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe'
-
+import { HttpClientModule } from "@angular/common/http";
 @NgModule({
   imports: [
     BrowserModule,
     FormsModule,
+    HttpClientModule,
   ],
   declarations: [
     AppComponent,
--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
@@ -13,12 +14,11 @@ export class ProductListComponent implements OnInit {
     imageShown: boolean = false;
     filteredBy: string;
     filteredProducts: IProduct[];
-
+    products: IProduct[];
     private _listFilter: string;
+    errorMsg = "";
 
-    constructor() {
-        this.listFilter = 'N/A';
-        this.filteredProducts = this.products;
+    constructor(private productService: ProductService) {
     }
 
     performFilter(filterBy: string): IProduct[] {
@@ -29,7 +29,13 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-        console.log('In OnInit..!');
+        this.productService.getProducts().subscribe({
+            next: products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error: err => this.errorMsg = err
+        });
     }
 
     toggleImage(): void {
@@ -49,36 +55,4 @@ export class ProductListComponent implements OnInit {
         this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
 
-    products: IProduct[] = [
-        {
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2019",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 3.2,
-            "imageUrl": "assets/images/leaf_rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2019",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "assets/images/garden_cart.png"
-        },
-        {
-            "productId": 5,
-            "productName": "Hammer",
-            "productCode": "TBX-0048",
-            "releaseDate": "May 21, 2019",
-            "description": "Curved claw steel hammer",
-            "price": 8.9,
-            "starRating": 4.8,
-            "imageUrl": "assets/images/hammer.png"
-        }
-    ];
 }
